23aed1a8c957f7be882d8c017af4d51294d1d917
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2022-12-28 17:15:41 -0300

    Update Persona.java

--- src/main/java/com/portfolio/Luciano/Entity/Persona.java
+++ src/main/java/com/portfolio/Luciano/Entity/Persona.java
@@ -6,8 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
 
 @Entity
@@ -16,15 +14,15 @@ public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+            
     @NotNull
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
     private String nombre;
-
+    
     @NotNull
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
     private String apellido;
-
+    
     @NotNull
     private String description;
     
